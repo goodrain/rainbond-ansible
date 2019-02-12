f55ecf5f006cbaec28c588f7f9b31183cbe1009d
ysicing zheng <i@ysicing.me>
2019-02-12 09:42:22 +0800

    [REV] fix client check storage

--- setup.sh
+++ setup.sh
@@ -15,20 +15,20 @@
 
 [[ $DEBUG ]] && set -ex || set -e
 
-installer_dir="$(dirname "${0}")"
+INSTALLER_DIR="$(dirname "${0}")"
 
 [ ! -d "/opt/rainbond/.init" ] && mkdir -p /opt/rainbond/.init
 
-if [ -f "${installer_dir}/scripts/installer/functions.sh" ]; then
-	source "${installer_dir}/scripts/installer/functions.sh" || notice "not found functions.sh"
+if [ -f "${INSTALLER_DIR}/scripts/installer/functions.sh" ]; then
+	source "${INSTALLER_DIR}/scripts/installer/functions.sh" || notice "not found functions.sh"
 fi
 
-if [ -f "${installer_dir}/scripts/installer/default.sh" ]; then
-	source "${installer_dir}/scripts/installer/default.sh" || notice "not found default.sh"
+if [ -f "${INSTALLER_DIR}/scripts/installer/default.sh" ]; then
+	source "${INSTALLER_DIR}/scripts/installer/default.sh" || notice "not found default.sh"
 fi
 
-if [ -f "${installer_dir}/scripts/installer/global.sh" ]; then
-	source "${installer_dir}/scripts/installer/global.sh" || notice "not found global.sh"
+if [ -f "${INSTALLER_DIR}/scripts/installer/global.sh" ]; then
+	source "${INSTALLER_DIR}/scripts/installer/global.sh" || notice "not found global.sh"
 fi
 
 [ -z "$IIP" ] && IIP=$1
@@ -50,7 +50,6 @@ get_default_config(){
         db=$(pwgen 8 1)
         [ ! -z "$db" ] &&  (
             echo "$db" > /opt/rainbond/.init/db
-            
         )
     )
     [ ! -f "/root/.ssh/id_rsa.pub" ] && (
@@ -74,20 +73,20 @@ get_default_dns() {
 }
 
 get_default_netwrok_type() {
-    if [ "$NETWORK_TYPE" == "flannel" ];then
+    if [ "$NETWORK_TYPE" == "flannel" ]; then
         network="flannel"
     else
         network="calico"
     fi
     info "Pod Network Provider" "${network}"
-    if [ -z "$POD_NETWORK_CIDR" ];then
+    if [ -z "$POD_NETWORK_CIDR" ]; then
         IP_INFO=$(ip ad | grep 'inet ' | egrep ' 10.|172.|192.168' | awk '{print $2}' | cut -d '/' -f 1 | grep -v '172.30.42.1')
         IP_ITEMS=($IP_INFO)
         INET_IP=${IP_ITEMS%%.*}
-        if [ "$NETWORK_TYPE" == "flannel" ];then
+        if [ "$NETWORK_TYPE" == "flannel" ]; then
             pod_network_cidr="${flannel_pod_network_cidr}"
         else
-            if [ "$INET_IP" != "192" ];then
+            if [ "$INET_IP" != "192" ]; then
                 pod_network_cidr="${calico_pod_network_cidr}"
             else
                 pod_network_cidr="${pod_network_cidr_10}"
@@ -125,15 +124,15 @@ Generate_domain(){
     DOMAIN_LOG="/opt/rainbond/.domain.log"
     AUTH=$(cat /opt/rainbond/.init/secretkey)
     echo "" > $DOMAIN_LOG
-    if [ -z "$DOMAIN" ];then
-    if [ "$INSTALL_TYPE" == "online" ];then
+    if [ -z "$DOMAIN" ]; then
+    if [ "$INSTALL_TYPE" == "online" ]; then
         curl -s --connect-timeout 20  -d 'ip='"$DOMAIN_IP"'&uuid='"$DOMAIN_UUID"'&type='"$DOMAIN_TYPE"'&auth='"$AUTH"'' -X POST  $DOMAIN_API/domain/new > $DOMAIN_LOG
         cat > /tmp/.lock.domain <<EOF
 curl -s --connect-timeout 20  -d 'ip='"$DOMAIN_IP"'&uuid='"$DOMAIN_UUID"'&type='"$DOMAIN_TYPE"'&auth='"$AUTH"'' -X POST  $DOMAIN_API/new > $DOMAIN_LOG
 EOF
     fi
     [ -f $DOMAIN_LOG ] && wilddomain=$(cat $DOMAIN_LOG )
-    if [[ "$wilddomain" == *grapps.cn ]];then
+    if [[ "$wilddomain" == *grapps.cn ]]; then
         info "wild-domain:" "$wilddomain"
         sed -i -r  "s/(^app_domain: ).*/\1$wilddomain/" roles/rainvar/defaults/main.yml
         [ ! -d "/opt/rainbond/bin" ] && mkdir -p /opt/rainbond/bin
@@ -164,7 +163,7 @@ up_domain_dns(){
         iip=$(cat /opt/rainbond/.init/domain.yaml | grep iip | awk '{print $2}')
         domain=$(cat /opt/rainbond/.init/domain.yaml | grep domain | awk '{print $2}')
         DOMAIN_API=$(cat /opt/rainbond/.init/domain.yaml | grep api | awk '{print $2}')
-        if [[ "$domain" =~ "grapps" ]];then
+        if [[ "$domain" =~ "grapps" ]]; then
             curl -s --connect-timeout 20 ${DOMAIN_API}/status\?uuid=$uid\&ip=$iip\&type=True\&domain=$domain >/dev/null 
         fi
     ) || (
@@ -178,10 +177,10 @@ copy_from_centos(){
     cp -a ./hack/chinaos/centos-release /etc/os-release
     mkdir -p /etc/yum.repos.d/backup >/dev/null 2>&1
     check_repo=$(ls /etc/yum.repos.d/*.repo 2>/dev/null | wc -l)
-    if [ "$check_repo" != 0 ];then
+    if [ "$check_repo" != 0 ]; then
         mv -f /etc/yum.repos.d/*.repo /etc/yum.repos.d/backup >/dev/null 2>&1
     else
-        #todo
+        # todo
         echo ""
     fi
     cp -a ./hack/chinaos/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo
@@ -199,10 +198,10 @@ centos_offline(){
     info "Remove default CentOS source"
     [ ! -d "/etc/yum.repos.d/backup" ] && mkdir -p /etc/yum.repos.d/backup
     check_repo=$(ls /etc/yum.repos.d/*.repo 2>/dev/null | wc -l)
-    if [ "$check_repo" != 0 ];then
+    if [ "$check_repo" != 0 ]; then
         mv -f /etc/yum.repos.d/*.repo /etc/yum.repos.d/backup >/dev/null 2>&1
     else
-        #todo
+        # todo
         echo ""
     fi
 }
@@ -240,7 +239,6 @@ online_init(){
             yum install -y epel-release 
             yum makecache fast 
             yum install -y sshpass python-pip uuidgen pwgen expect
-            # fix centos 7.5 pip install ImportError
             pip install -U setuptools -i https://pypi.tuna.tsinghua.edu.cn/simple
 		;;
 		*)
@@ -248,9 +246,7 @@ online_init(){
 		;;
     esac
     export LC_ALL=C
-    # pip install setuptools pip -U -i https://pypi.tuna.tsinghua.edu.cn/simple
     pip install ansible -i https://pypi.tuna.tsinghua.edu.cn/simple
-
 }
 
 offline_init(){
@@ -295,7 +291,7 @@ get_default_install_type(){
     info "Install Type" "$INSTALL_TYPE"
     sed -i -r  "s/(^install_type: ).*/\1$INSTALL_TYPE/" roles/rainvar/defaults/main.yml
     sed -i -r  "s/(^deploy_type: ).*/\1$DEPLOY_TYPE/" roles/rainvar/defaults/main.yml
-    if [ "$INSTALL_TYPE" == "online" ];then
+    if [ "$INSTALL_TYPE" == "online" ]; then
         online_init_docker_version
         online_init
     else
@@ -306,11 +302,10 @@ get_default_install_type(){
 check_port(){
     local portlist=(53 80 443 3306)
     local check_fail_num=0
-    for port in ${portlist[@]}
-    do
+    for port in ${portlist[@]}; do
         netstat -pantu | awk '{print $4}' | awk -F: '{print $2}' | sort -ru | grep "\b$port\b" >> /tmp/check_port_log && ((check_fail_num+=1)) || sleep 1
     done
-    if [ "$check_fail_num" == 0 ];then
+    if [ "$check_fail_num" == 0 ]; then
 	    touch /opt/rainbond/.init/.port_check
     else
         notice "port is already used, please check port in: ${portlist[@]}"
@@ -321,10 +316,10 @@ check_disk(){
     local disk=$(df -h | grep "/$" | awk '{print $2}' | tr 'G' ' ')
     DISK_LIMIT=40
     DISK_STATUS=$(awk -v num1=$disk -v num2=$DISK_LIMIT 'BEGIN{print(num1>=num2)?"0":"1"}')
-    if [ "$DISK_STATUS" == '0' ];then
+    if [ "$DISK_STATUS" == '0' ]; then
         info "prepare check disk" "passed"
     else
-        if [ "$ENABLE_CHECK" == "enable" ];then
+        if [ "$ENABLE_CHECK" == "enable" ]; then
             notice "The disk is recommended to be at least 40GB"
         else
             info "!!! Skip disk check.The disk is recommended to be at least 40GB(now ${disk}GB)"
@@ -335,7 +330,7 @@ check_disk(){
 
 precheck(){
     progress "Prepare check"
-    if [ ! -f "/opt/rainbond/.init/.port_check" ];then
+    if [ ! -f "/opt/rainbond/.init/.port_check" ]; then
         check_port
     fi
     info "prepare check port" "passed"
@@ -355,7 +350,7 @@ show_succeed(){
 onenode(){
     progress "Initialize the data center"
     ansible-playbook -i inventory/hosts setup.yml
-    if [ "$?" -eq 0 ];then
+    if [ "$?" -eq 0 ]; then
         curl -Is 127.0.0.1:7070 | head -1 | grep 200 > /dev/null && progress "Congratulations on your successful installation" || sleep 1
         show_succeed
     else
@@ -371,7 +366,7 @@ multinode(){
 thirdparty(){
     progress "Only Install Rainbond On Thirdparty Node"
     ansible-playbook -i inventory/hosts hack/thirdparty/setup.yaml
-    if [ "$?" -eq 0 ];then
+    if [ "$?" -eq 0 ]; then
         show_succeed
     else
         notice "The installation did not succeed, please redo it or ask for help"
@@ -392,16 +387,14 @@ prepare(){
     get_default_config
     [ ! -z "$EIP" ] && Generate_domain $EIP $VIP || Generate_domain $IIP $VIP
     hname=$(hostname -s)
-    if [ "$ROLE" == "master" -o "$ROLE" == "manage" ];then 
+    if [ "$ROLE" == "master" -o "$ROLE" == "manage" ]; then 
         cp inventory/hosts.master inventory/hosts
     else
         cp inventory/hosts.all inventory/hosts
     fi
     sed -i "s#node1#$hname#g" inventory/hosts
     sed -i "s#10.10.10.13#$IIP#g" inventory/hosts
-
     [ ! -z "$ENABLE_CHECK" ] && sed -i -r  "s/(^enable_check: ).*/\1$ENABLE_CHECK/" roles/rainvar/defaults/main.yml || echo ""
-
 }
 
 update_etcd(){
